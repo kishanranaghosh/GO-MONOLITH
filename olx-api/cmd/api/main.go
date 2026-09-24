3e97b37f06758a1ed2ae75bbb41b78cab7b3c6a0
kishan <kishan@kishans-Mac-mini.local>
2026-09-24 18:58:22 +0530

    create server

--- olx-api/cmd/api/main.go
+++ olx-api/cmd/api/main.go
@@ -2,49 +2,26 @@ package main
 
 import (
 	"log"
-	"log/slog"
 	"net/http"
-	"os"
 	"time"
 
-	"github.com/kishanghosh090/GO-MONOLITH/internal/config"
-	"github.com/kishanghosh090/GO-MONOLITH/internal/db"
 	"github.com/kishanghosh090/GO-MONOLITH/internal/handlers"
 )
 
 func main() {
-	// load env
-	cfg := config.MustLoad()
-	db, err := db.Connect(cfg.DatabaseUrl)
-	if err != nil {
-		log.Fatalf("main.db.Connect: %v", err)
-	}
-
-	handler := slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
-		AddSource: true,
-		Level:     slog.LevelDebug,
-	})
-
-	logger := slog.New(handler)
-	slog.SetDefault(logger)
-
-	lh := handlers.NewListingHandler(db, logger)
-
 	mux := http.NewServeMux()
 	mux.HandleFunc("GET /healthz", handlers.Health)
-	mux.HandleFunc("GET /listings", lh.List)
-	mux.HandleFunc("DELETE /listings/{id}", lh.Delete)
 
+	// set server configs
 	srv := http.Server{
-		Addr:         ":" + cfg.Port,
+		Addr:         ":8090",
 		Handler:      mux,
-		ReadTimeout:  time.Second * 10,
-		WriteTimeout: time.Second * 30,
+		ReadTimeout:  time.Minute * 2,
+		WriteTimeout: time.Minute * 5,
 		IdleTimeout:  time.Second * 60,
 	}
-	log.Printf("Server is listing on %s", srv.Addr)
 	if err := srv.ListenAndServe(); err != nil {
-		log.Fatalf("Server failed: %v", err)
+		log.Fatalf("server failed: %v", err)
 	}
 
 }
